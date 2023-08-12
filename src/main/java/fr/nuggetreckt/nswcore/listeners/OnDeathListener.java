1a492d5c473f40289f0fbb602c6b866e01413bdf
NuggetReckt <corto.morrow@gmail.com>
2023-08-12 15:24:30 +0200

    Added: New death messages
    TODO: Hide/Show resolved reports button
    TODO: Tri des reports par date > et <, nom du joueur report alphabétique > et <
    TODO: algo de "routage" d'id des reports afin d'éviter les erreurs

--- src/main/java/fr/nuggetreckt/nswcore/listeners/OnDeathListener.java
+++ src/main/java/fr/nuggetreckt/nswcore/listeners/OnDeathListener.java
@@ -10,6 +10,8 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
@@ -24,19 +26,25 @@ public class OnDeathListener implements Listener {
         DamageCause damageCause = Objects.requireNonNull(entityDamageEvent).getCause();
         Random r = new Random();
 
+        List<MessageManager> deathMessages = new ArrayList<>();
+        String deathMessage;
+
         //TODO: add (if player spawn/home is not set) tp on player die to spawn
         switch (damageCause) {
             case ENTITY_ATTACK, ENTITY_SWEEP_ATTACK, ENTITY_EXPLOSION, PROJECTILE -> {
                 if (entityDamageEvent instanceof EntityDamageByEntityEvent) {
                     Entity damager = ((EntityDamageByEntityEvent) player.getLastDamageCause()).getDamager();
                     if (damager instanceof Player) {
-                        int a = r.nextInt(0, 1);
+                        deathMessages.add(MessageManager.PLAYER_KILL);
+                        deathMessages.add(MessageManager.PLAYER_KILL2);
+                        deathMessages.add(MessageManager.PLAYER_KILL3);
+                        deathMessages.add(MessageManager.PLAYER_KILL4);
 
-                        if (a == 0) {
-                            event.setDeathMessage(String.format(MessageManager.PLAYER_KILL.getDeathMessage(), player.getName(), damager.getName()));
-                        } else {
-                            event.setDeathMessage(String.format(MessageManager.PLAYER_KILL2.getDeathMessage(), player.getName(), damager.getName()));
-                        }
+                        int a = r.nextInt(0, deathMessages.size());
+                        deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName(), damager.getName());
+
+                        event.setDeathMessage(deathMessage);
+                        deathMessages.clear();
                     } else if (damager instanceof Creeper) {
                         event.setDeathMessage(String.format(MessageManager.PLAYER_DEATH_CREEPER.getDeathMessage(), player.getName()));
                     } else if (damager instanceof Projectile) {
@@ -49,27 +57,100 @@ public class OnDeathListener implements Listener {
                     }
                 }
             }
-            case LAVA, FIRE, FIRE_TICK, HOT_FLOOR ->
-                    event.setDeathMessage(String.format(MessageManager.PLAYER_DEATH_BURNED.getDeathMessage(), player.getName()));
-            case DROWNING ->
-                    event.setDeathMessage(String.format(MessageManager.PLAYER_DEATH_DROWNED.getDeathMessage(), player.getName()));
-            case SUFFOCATION -> {
+            case LAVA, FIRE, FIRE_TICK, HOT_FLOOR -> {
+                deathMessages.add(MessageManager.PLAYER_DEATH_BURNED);
+                deathMessages.add(MessageManager.PLAYER_DEATH_BURNED2);
 
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
+            }
+            case DROWNING -> {
+                deathMessages.add(MessageManager.PLAYER_DEATH_DROWNED);
+                deathMessages.add(MessageManager.PLAYER_DEATH_DROWNED2);
+                deathMessages.add(MessageManager.PLAYER_DEATH_DROWNED3);
+
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
+            }
+            case SUFFOCATION, FALLING_BLOCK -> {
+                deathMessages.add(MessageManager.PLAYER_DEATH_SUFFOCATION);
+                deathMessages.add(MessageManager.PLAYER_DEATH_SUFFOCATION2);
+
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
             }
             case POISON -> {
+                deathMessages.add(MessageManager.PLAYER_DEATH_MAGIC);
+                deathMessages.add(MessageManager.PLAYER_DEATH_POISON);
 
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
             }
-            case FALL, FLY_INTO_WALL ->
-                    event.setDeathMessage(String.format(MessageManager.PLAYER_DEATH_FALL.getDeathMessage(), player.getName()));
-            case VOID ->
-                    event.setDeathMessage(String.format(MessageManager.PLAYER_DEATH_VOID.getDeathMessage(), player.getName()));
+            case MAGIC -> {
+                deathMessages.add(MessageManager.PLAYER_DEATH_MAGIC);
+                deathMessages.add(MessageManager.PLAYER_DEATH_MAGIC2);
+
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
+            }
+            case FALL, FLY_INTO_WALL -> {
+                deathMessages.add(MessageManager.PLAYER_DEATH_FALL);
+                deathMessages.add(MessageManager.PLAYER_DEATH_FALL2);
+                deathMessages.add(MessageManager.PLAYER_DEATH_FALL3);
+                deathMessages.add(MessageManager.PLAYER_DEATH_FALL4);
+                deathMessages.add(MessageManager.PLAYER_DEATH_FALL5);
+
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
+            }
+            case VOID -> {
+                deathMessages.add(MessageManager.PLAYER_DEATH_VOID);
+                deathMessages.add(MessageManager.PLAYER_DEATH_VOID2);
+
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
+            }
+            case BLOCK_EXPLOSION, LIGHTNING -> {
+                deathMessages.add(MessageManager.PLAYER_DEATH_EXPLOSION);
+                deathMessages.add(MessageManager.PLAYER_DEATH_EXPLOSION2);
+
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
+            }
             default -> {
-                int a = r.nextInt(0, 1);
-                if (a == 0) {
-                    event.setDeathMessage(String.format(MessageManager.PLAYER_DEATH.getDeathMessage(), player.getName()));
-                } else {
-                    event.setDeathMessage(String.format(MessageManager.PLAYER_DEATH2.getDeathMessage(), player.getName()));
-                }
+                deathMessages.add(MessageManager.PLAYER_DEATH);
+                deathMessages.add(MessageManager.PLAYER_DEATH2);
+                deathMessages.add(MessageManager.PLAYER_DEATH3);
+
+                int a = r.nextInt(0, deathMessages.size());
+                deathMessage = String.format(deathMessages.get(a).getDeathMessage(), player.getName());
+
+                event.setDeathMessage(deathMessage);
+                deathMessages.clear();
             }
         }
     }
