11120c3bac510b3d21c68b4b0ffba4e1991ed8a4
NuggetReckt <corto.morrow@gmail.com>
2024-03-26 01:16:53 +0100

    Fixed: Dragon egg will not spawn on dragon kill

--- src/main/java/fr/nuggetreckt/nswcore/listeners/OnDragonDeathListener.java
+++ src/main/java/fr/nuggetreckt/nswcore/listeners/OnDragonDeathListener.java
@@ -4,33 +4,44 @@ import fr.nuggetreckt.nswcore.NSWCore;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.boss.DragonBattle;
 import org.bukkit.entity.EnderDragon;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 
 public class OnDragonDeathListener implements Listener {
 
+    private BukkitTask runnable;
+
     @EventHandler
     public void onDragonEggDrop(@NotNull EntityDeathEvent event) {
         if (event.getEntity() instanceof EnderDragon) {
+            DragonBattle battle = ((EnderDragon) event.getEntity()).getDragonBattle();
+
+            assert battle != null;
+            if (battle.hasBeenPreviouslyKilled()) {
+                return;
+            }
             World endWorld = event.getEntity().getWorld();
 
-            new BukkitRunnable() {
+            runnable = new BukkitRunnable() {
                 @Override
                 public void run() {
-                    for (int i = 58; i < 72; i++) {
-                        Block block = endWorld.getBlockAt(0, i, 0);
+                    for (int y = 50; y < 100; y++) {
+                        Block block = endWorld.getBlockAt(0, y, 0);
 
                         if (block.getType() == Material.DRAGON_EGG) {
                             block.setType(Material.AIR);
+                            runnable.cancel();
                             return;
                         }
                     }
                 }
-            }.runTaskLater(NSWCore.getInstance(), 202L);
+            }.runTaskTimer(NSWCore.getInstance(), 1L, 20L);
         }
     }
 }
